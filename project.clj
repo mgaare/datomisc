0ed128e258e6c44d7f5f9d304925be12637a016c
Michael Gaare <michael.gaare@gmail.com>
2015-03-21 18:12:37 -0400

    Bump version, add description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject datomisc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject datomisc "0.1.0"
+  :description "The Datomic utility functions you're tired having to write."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
