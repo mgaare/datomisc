63b25b6d90e9893dd8a482cdd0dd193cb45d2a8d
Michael Gaare <michael.gaare@gmail.com>
2015-12-21 13:56:14 -0500

    Clojure 1.7

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "The Datomic utility functions I'm tired having to write."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  ;; exclude me if you want to use pro
                  [com.datomic/datomic-free "0.9.5344"]]
   :profiles {:dev {:source-paths ["dev"]
