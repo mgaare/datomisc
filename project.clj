a4b7e5052d7af98ce974a0daced1ddb4f8828153
Michael Gaare <michael.gaare@gmail.com>
2015-12-21 13:55:00 -0500

    Remove cider-nrepl middleware
    - Shame on me, this should never have been in here

--- project.clj
+++ project.clj
@@ -6,5 +6,4 @@
                  ;; exclude me if you want to use pro
                  [com.datomic/datomic-free "0.9.5130"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
                    :dependencies [[org.clojure/tools.namespace "0.2.10"]]}})
